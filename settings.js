6a7241d2218c5406d87aa7c81d2f33be2327659a
Ishan20081124 <dammikaishan31@gmail.Com>
2024-12-28 05:16:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-PREFIX: process.env.PREFIX || '.' ,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=KqRh3bxC#nIJcRjZlFitl-gEth3ap25LUJflXDQ0EVjNYEmmkG2g' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '0752230618' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
